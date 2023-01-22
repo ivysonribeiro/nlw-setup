a4993e6f230362767b77e7618fa12f6d47a87e50
Ivyson Ribeiro <ribeiroivyson@yahoo.com>
2023-01-22 19:09:40 -0300

    configurando JS

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert('Olá!')
+const form = document.querySelector('form')
+const nlwSetup = new NLWSetup(form)
+
+const data = {
+  run: ['01-01', '01-02', '01-06']
+}
+
+nlwSetup.setData(data)
+nlwSetup.load()
