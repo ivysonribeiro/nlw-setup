836243a33347e66102df5c22b075b10aba516c71
Ivyson Ribeiro <ribeiroivyson@yahoo.com>
2023-01-25 02:28:12 -0300

    configurando JS

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 const form = document.querySelector('form')
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-  run: ['01-01', '01-02', '01-06']
+button.addEventListener('click', add)
+
+function add() {
+  
+  const today = '01/01'
+  const dayExists = nlwSetup.dayExists(today)
+
+  if(dayExists) {
+    alert('Dia já Incluso!')
+    return
+  }
+
+   alert('Adiconado com sucesso ✅') 
+   nlwSetup.addDay(today)
 }
 
-nlwSetup.setData(data)
-nlwSetup.load()
+//const data = {
+ // run: ['01-01', '01-02', '01-06', '01-07', '01-08'],
+ // takePills: ['01-03'],
+ // journal: ['01-02']
+
+//}
+
+//nlwSetup.setData(data)
+//nlwSetup.load()
